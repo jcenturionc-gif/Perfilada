0ba8b41ee52366a6c68761d4a5a1d3463a8b1484
jcenturionc-gif <jordanh.centurionc@gmail.com>
2026-08-14 13:07:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+//======================================
+// CONFIGURACIÓN GENERAL
+//======================================
+
+const CONFIG = {
+
+  // URL de Google Apps Script
+  API_URL: "https://script.google.com/macros/s/AKfycbyoROWs4xbSSdxA3r8m3VjsJoR8zB5VeOmWb81dC_tkIKwICea2Mhplm91SH3_ahw/exec",
+
+  // Metas del negocio
+  METAS: {
+
+      CONVERSION: 80,
+      PRODUCTIVIDAD: 2,
+      ADHERENCIA: 95,
+      CUMPLIMIENTO: 100
+
+  },
+
+  // Colores corporativos
+  COLORES: {
+
+      AZUL: "#0057B8",
+      VERDE: "#16a34a",
+      ROJO: "#dc2626",
+      AMARILLO: "#f59e0b",
+      CELESTE: "#0ea5e9",
+      GRIS: "#6b7280"
+
+  }
+
+};
